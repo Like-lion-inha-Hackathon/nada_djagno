413e77d4a01b6ed4695c24122c75341ad5f5b97f
Minhyeok <sonmh79@naver.com>
2021-08-07 20:34:29 +0900

    try backend ㅠㅠ
    try json ,,

--- core/views.py
+++ core/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from django.core import serializers
 import json
 from users import models as user_models
+from django.http import JsonResponse
 
 # Create your views here.
 
 
 def intro_view(request):
-    return render(request, "intro.html")
+    return render(request, "try.html")
 
 
 def try_view(request):
-    users = user_models.User.objects.all()
-    users = serializers.serialize("json", users)
+    if request.GET:
+        return render(request, "try.html")
 
-    return render(request, "try.html", {"users": users})
+    users = user_models.User.objects.all()
+    user_list = []
+    for user in users:
+        user_dict = {"id": user.id, "name": user.nickname}
+        user_list.append(user_dict)
+
+    return JsonResponse({"user_list": user_list})
